d44ac46807db3f5fae7be7265a4b62b7167e7c43
leshka <leshkajm@ya.ru>
2017-09-26 12:06:35 +0300

    Big update: 	Changed bot's core code to work with all channel stats/configs in one db. 	Improved main loop performance. 	New configuration system, easier syntax and more configurable options per pickup. 	Different !noadd syntax. 	Made sub request messages fully configurable. 	Made '!maps', '!map' and '!start' always work if only one pickup is configured on the channel. 	Added link to channel in pm message on a pickup start. 	Added player count to !who command reply. 	!who now prints nicks rather than usernames if possible. 	Now pickup start will remove added players from pickups on other servers channels too. 	!expire, !default_expire and !allowoffline now have global effect (on all Pubobot's channels). 	!promote now promotes pickup with most players added by default. 	Added !top daily. 	Decreased delay between sending messages by twice. 	Removed backups feature. 	More appealing teams reply and finalized teams message. (also added random animal smiley as a team indicator =)). 	Added '!put' command for moderators/admins (teams picking). 	Added captains_role feature (random captains will be preffered to this role, also '!capfor' command will be only availible for users with this role if its set). 	Added teams picking order feature. 	Added moderator role (role wich will allow minor admin commands like !noadd or !put etc). 	Added pickup groups feature. 	Added !reset_stats command. 	Add matches objects, witch will allow:					#DONE 		Match reports, rankings by points and !fatkid.		#WIP 		More than one 'active' pug per time on a channel.	#DONE 		!ready feature.										#DONE 		Denying players to !add while in pug.				#DONE 	Added script to convert old databases to new. 	Bugfix: Fixed highlight handling on users with changed nicks. 	Bugfix: Fixed bug @ !remove_pickups did not remove pickups from the database. 	Bugfix: Fixed bug wich allowed creating same pickup with different letters case. 	Bugfix: Fixed bug @ bot's status is gone after reconnect.

---
+++ updater.py
@@ -0,0 +1,138 @@
+#!/usr/bin/python3
+# encoding: utf-8
+import os, imp, sqlite3, operator, discord, asyncio
+
+def get_role_id(channel, role_name):
+	if role_name:
+		for role in channel.server.roles:
+			if role.name.lower() == role_name.lower():
+				return role.id
+	return None
+
+def update_databases():
+	newconn = sqlite3.connect("database.sqlite3")
+	newc = newconn.cursor()
+	for channel_id in os.listdir("channels"):
+		if channel_id != 'default':
+			channel = c.get_channel(channel_id)
+			if not channel:
+				print("Could not find channel with channel id '{0}', scipping...".format(channel_id))
+			else:
+				oldconn = sqlite3.connect("channels/{0}/stats.sql".format(channel_id))
+				oldc = oldconn.cursor()
+				oldc.execute("SELECT variable, value FROM config")
+				l = list(oldc.fetchall())
+				for i in l:
+					i = list(i)
+					if i[1].lower() == 'none':
+						i[1] = None
+					if i[0] == "ADMINROLE":
+						admin_role = get_role_id(channel, i[1])
+					elif i[0] == "DEFAULT_IP":
+						if i[1] == "127.0.0.1":
+							ip = None
+						else:
+							ip = i[1]
+					elif i[0] == "PICKUP_PASSWORD":
+						password = i[1]
+					elif i[0] == "IP_FORMAT":
+						startmsg = i[1]
+					elif i[0] == "FIRST_INIT":
+						first_init = i[1]
+					elif i[0] == "BANTIME":
+						default_bantime = int(i[1])*60*60
+					elif i[0] == "CHANNEL_NAME":
+						channel_name = i[1]
+					elif i[0] == "ADMINID":
+						admin_id = i[1]
+					elif i[0] == "PROMOTION_DELAY":
+						promotion_delay = int(i[1])*60
+					elif i[0] == "PROMOTION_ROLE":
+						promotion_role = get_role_id(channel, i[1])
+					elif i[0] == "PREFIX":
+						prefix = i[1]
+					elif i[0] == "++_REQ_PLAYERS":
+						req_players = int(i[1])
+					elif i[0] == "FIRST_INIT":
+						first_init = int(i[1])
+					elif i[0] == "TEAMS_PICK_SYSTEM":
+						if i[1] == "MANUAL_PICK":
+							pick_captains = False
+							pick_teams = "manual"
+						elif i[1] == "CAPTAINS_PICK":
+							pick_captains = True
+							pick_teams = "manual"
+						elif i[1] == "RANDOM_TEAMS":
+							pick_captains = False
+							pick_teams = "auto"
+						else:
+							pick_captains = True
+							pick_teams = "no_teams"
+				
+				print("UPDATING CHANNEL '{0}' id '{1}'...".format(channel_name, channel_id))
+						
+				newc.execute("INSERT INTO channels (channel_id, channel_name, first_init, admin_id, admin_role, prefix, default_bantime, `++_req_players`, startmsg, ip, password, pick_captains, pick_teams, promotion_role, promotion_delay) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", (channel_id, channel_name, first_init, admin_id, admin_role, prefix, default_bantime, req_players, startmsg, ip, password, pick_captains, pick_teams, promotion_role, promotion_delay))
+			
+				oldc.execute("SELECT name, maxplayers, ip, promotion_role, whitelist_role, blacklist_role, maps FROM pickups_config")
+				l = list(oldc.fetchall())
+				for i in l:
+					i = list(i)
+					if i[2] == "127.0.0.1":
+						i[2] = None
+					if i[3] == "none":
+						i[3] = None
+					if i[4] == "none":
+						i[4] = None
+					if i[5] == "none":
+						i[5] = None
+					newc.execute("INSERT INTO pickup_configs (channel_id, pickup_name, maxplayers, ip, promotion_role, whitelist_role, blacklist_role, maps) VALUES (?, ?, ?, ?, ?, ?, ?, ?)", (channel_id, i[0], i[1], i[2], i[3], i[4], i[5], i[6]))
+			
+				oldc.execute("SELECT memberid, phrase FROM players")
+				l = list(oldc.fetchall())
+				for i in l:
+					i = list(i)
+					if i[1].lower() == "false":
+						i[1] = None
+					newc.execute("INSERT INTO channel_players (channel_id, user_id, phrase) VALUES (?, ?, ?)", (channel_id, i[0], i[1]))
+
+				oldc.execute("SELECT id, time, gametype, players FROM pickups")
+				l = oldc.fetchall()
+				for i in l:
+					newc.execute("INSERT INTO pickups (channel_id, pickup_id, at, pickup_name, players) VALUES (?, ?, ?, ?, ?)", (channel_id, i[0], i[1], i[2], i[3]))
+
+				oldc.execute("SELECT pickup_id, memberid, membername, time, gametype FROM player_games")
+				l = oldc.fetchall()
+				for i in l:
+					newc.execute("INSERT INTO player_pickups (channel_id, pickup_id, user_id, user_name, at, pickup_name) VALUES (?, ?, ?, ?, ?, ?)", (channel_id, i[0], i[1], i[2], i[3], i[4]))
+				oldconn.close()
+
+	newconn.commit()
+	newconn.close()
+
+
+c = discord.Client()
+loop = asyncio.get_event_loop()
+
+@c.event
+@asyncio.coroutine
+def on_ready():
+	update_databases()
+	print("SUCCESSFULLY UPDATED")
+	c.close()
+	os.rename("channels", "channels_old")
+	os._exit()
+		
+f = open('config.cfg', 'r')
+cfg = imp.load_source('data', '', f)
+f.close()
+
+try:
+	if cfg.DISCORD_TOKEN != "":
+		print("SYSTEM| logging in with token...")
+		loop.run_until_complete(c.login(cfg.DISCORD_TOKEN))
+	else:
+		print("SYSTEM| logging in with username and password...")
+		loop.run_until_complete(c.login(cfg.USERNAME, cfg.PASSWORD))
+	loop.run_until_complete(c.connect())
+except:
+	pass
